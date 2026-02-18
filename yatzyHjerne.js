eedcc014837325269230c24743e46fe81c1e5418
krillebc-school <eaa25cbc@students.eaaa.dk>
2026-02-18 09:19:53 +0100

    test

---
+++ yatzyHjerne.js
@@ -0,0 +1,10 @@
+let values = [
+    {val: null , hold: false, roll: roll },
+    {val: null , hold: false, roll: roll},
+    {val: null , hold: false, roll: roll},
+    {val: null , hold: false, roll: roll},
+    {val: null , hold: false, roll: roll}
+
+]
+
+let turn = 0;
